249aad017dba2385856c1f3c3852664ef0722330
Slezi94 <slezak.bence94@gmail.com>
2025-05-13 15:04:00 +0200

    AAFE60E & EFT50_58E exception in str modification

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ def modify_attributes(ana_csv, lua_csv, do_csv, output):
     # Csak azok a sorok, ahol attribútum 0
 
     zero_attr_rows = output[(output["Item.Attribute.0.Value"] == 0) & (output["PriceRule.Code"] != "ZeroPrice")]
-    #print(zero_attr_rows)
     # Új sorokat ide gyűjtjük
     new_rows = []
 
@@ -20,24 +19,16 @@ def modify_attributes(ana_csv, lua_csv, do_csv, output):
         matching_rows = all_prices[all_prices["sku"] == ref]
 
         if not matching_rows.empty:
-            #print(matching_rows)
             for _, match in matching_rows.iterrows():
                 new_row = row.copy()
                 new_row["Item.Attribute.0.Value"] = match["attribute"]
                 new_rows.append(new_row)
-    #print(f"új sorok: {new_rows}")
+
     new_rows = pandas.DataFrame(data=new_rows)
-    # TODO: A nem kívánt sorok törlése
-    #mask = ~((output["Item.Attribute.0.Value"] == 0) & (output["Item.Cleared.RefCode"] == new_rows["Item.Cleared.RefCode"]))
     mask = ~zero_attr_rows["Item.Cleared.RefCode"].isin(new_rows["Item.Cleared.RefCode"])
     zero_attr_rows = zero_attr_rows[mask]
     zero_attr_rows = pandas.concat([zero_attr_rows, new_rows], ignore_index=True)
-    #output = output[mask]
-    print(f"pina:{zero_attr_rows}")
 
-    # Új sorok hozzáadása
-    '''output = pandas.concat([output, pandas.DataFrame(new_rows)], ignore_index=True)
-    output.to_csv("Output/output.csv", index=False)'''
     return zero_attr_rows
 
 # CSV fájlok generálása Inputból
@@ -65,11 +56,19 @@ output_file.to_csv(f"Backup/output_backup_{timestamp}.csv", index=False)
 formatted_refCode = []
 
 for row in refCode:
-    if isinstance(row, str):
+    '''if isinstance(row, str):
         for suffix in ["_J", "_B", "_K"]:
             if suffix in row:
                 row = row[:row.rfind(suffix)]
-                break
+                break'''
+    if isinstance(row, str):
+        for suffix in ["_J", "_B", "_K"]:
+            if (row == "AAFE60") or (row == "EFT50_68"):
+                row.strip(suffix)
+                row = row+"E"
+            elif suffix in row:
+                row = row[:row.rfind(suffix)]
+
     formatted_refCode.append(row)
 
 # Segéd oszlop létrehozása
@@ -78,13 +77,10 @@ output_file.to_csv("Output/output.csv", index=False)
 
 output_file["ItemPrice"] = None
 
-#TODO:A 0-ás attribute-ok nál felvenni ugyan azokat a sorokat, megváltoztatni az attribútumát és felvenni az árát
-
 zero_attr_df = modify_attributes(ana, lua, do, output_file)
-#print(f"zéró attribútumok: {zero_attr_df}")
 mask = ~output_file["Item.Cleared.RefCode"].isin(zero_attr_df["Item.Cleared.RefCode"])
 output_file = output_file[mask]
-print(output_file)
+
 #Összeillesztés
 
 output_file = pandas.concat([zero_attr_df, output_file], join="inner", ignore_index=True)
@@ -125,8 +121,7 @@ output_file["ItemPrice"] = output_file["ItemPrice"].fillna(0)
 # Ár egész számmá alakítása
 output_file["ItemPrice"] = output_file["ItemPrice"].astype(int)
 
-#TODO: Segéd oszlopot törölni
-
+# Item.Cleared.RefCode oszlopot törölni
 #output_file = output_file.drop(columns=["Item.Cleared.RefCode"])
 
 # CSV mentése
